4ff0a13a5e9408098c3fe1c1fd43d5ab5b4a3eb5
Rubén <ruben.pm@pm.me>
2024-03-24 11:48:39 +0100

    Menor

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'gi://Gtk?version=4.0';
 import { Application } from './Application.js';
 
 export function main(argv) {
-	console.log("Nombre de la aplicación: " + pkg.name);
+	console.log("Nombre del paquete: " + pkg.name);
 	return new Application({ 'application-id': pkg.name }).run(argv);
 }
 
