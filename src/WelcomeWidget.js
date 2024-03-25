a45df618cbce7eb0650ad5a6bdf24b89995f1bd9
Rubén <ruben.pm@pm.me>
2024-03-25 23:11:06 +0100

    Pruebas lunes - 032504

--- src/WelcomeWidget.js
+++ src/WelcomeWidget.js
@@ -3,6 +3,36 @@ import Gtk from 'gi://Gtk';
 
 export const WelcomeWidget = GObject.registerClass({
 	GTypeName: 'FbrWelcomeWidget',
-	Template: 'resource:///es/ruven/copymag/ui/WelcomeWidget.ui'
-}, class extends Gtk.Widget {});
+	Template: 'resource:///es/ruven/copymag/ui/WelcomeWidget.ui',
+	Properties: {
+		WelcomeText: GObject.ParamSpec.string(
+			'welcome-text', // name
+			'Welcome Text', // nick
+			'The text displayed by the widget', // blurb
+			GObject.ParamFlags.READWRITE, // flags
+			'' // default value
+		),
+	},
+	InternalChildren: ['welcomeLabel'],
+}, class extends Gtk.Widget {
+	get welcomeText() {
+		// Return a default value if the text hasn't been set yet
+		return this._welcomeText || '';
+	}
+
+	set welcomeText(value) {
+		//value = "Ola ke ase";
+		// Do nothing if the new value is the same as the old one
+		if (this._welcomeText === value)
+			return;
+		// Store the value in an internal property
+		this._welcomeText = value;
+		// Notify that the value has changed
+		this.notify('welcome-text');
+		// Hide the label if no text is set
+		this._welcomeLabel.visible = !!value;
+		// Notify that the value has changed
+		this.notify('welcome-text');
+	}
+});
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import 'gi://Gtk?version=4.0';
 
 import { Application } from './Application.js';
 
+pkg.initGettext();
+
 export function main(argv) {
 	console.log("Nombre del paquete: " + pkg.name);
 	return new Application({ 'application-id': pkg.name }).run(argv);
 }
-
-
-
